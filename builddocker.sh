46de7031fe82093733dd2d9572d176d77d5ac2f6
xltxlm <xltxlm@qq.com>
2020-03-12 17:25:53 +0800

    注释

--- builddocker.sh
+++ builddocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t  registry.cn-shanghai.aliyuncs.com/copygithub/github:gitdebugdemo-5e689d00e6b1c1583914240 ./;
+docker build -t  registry.cn-shanghai.aliyuncs.com/copygithub/github:gitdebugdemo-5e689c8f994631583914127 ./;
 
-docker push registry.cn-shanghai.aliyuncs.com/copygithub/github:gitdebugdemo-5e689d00e6b1c1583914240;
+docker push registry.cn-shanghai.aliyuncs.com/copygithub/github:gitdebugdemo-5e689c8f994631583914127;
 
